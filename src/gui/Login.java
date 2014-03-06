5e64ae9bc89ca65ad129c76eb0e46a1df7379a43
robinsjovoll@hotmail.com <robinss@stud.ntnu.no>
2014-03-06 13:35:27 +0100

    Login gui made and working.

---
+++ src/gui/Login.java
@@ -0,0 +1,84 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField usernameField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		usernameField = new JTextField();
+		usernameField.setBounds(121, 54, 200, 20);
+		contentPane.add(usernameField);
+		usernameField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(121, 89, 200, 20);
+		contentPane.add(passwordField);
+		
+		JLabel usernameLabel = new JLabel("Username:");
+		usernameLabel.setBounds(22, 56, 89, 17);
+		contentPane.add(usernameLabel);
+		
+		JLabel passwordLabel = new JLabel("Password:");
+		passwordLabel.setBounds(22, 91, 89, 17);
+		contentPane.add(passwordLabel);
+		
+		JButton loginButton = new JButton("Login");
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		loginButton.setBounds(121, 120, 89, 23);
+		contentPane.add(loginButton);
+		
+		JButton newuserButton = new JButton("New user");
+		newuserButton.setBounds(232, 120, 89, 23);
+		contentPane.add(newuserButton);
+		
+		JButton FYPButton = new JButton("Forgot your password?");
+		FYPButton.setBounds(121, 154, 200, 28);
+		contentPane.add(FYPButton);
+	}
+}
