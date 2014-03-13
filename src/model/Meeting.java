f7288af30c0aff6a53654b84b60238484740d69c
Simon Borøy-Johnsen <simon_bj@live.com>
2014-03-13 15:47:29 +0100

    Added getAvailableRoom method. Added method for listing available rooms in new class SpecialQueries. Fixed getDuration method.

--- src/model/Meeting.java
+++ src/model/Meeting.java
@@ -1,16 +1,24 @@
 package model;
 
-import java.util.ArrayList;
+import com.sun.swing.internal.plaf.synth.resources.synth_sv;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Meeting {
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 	private final int meetingID;
 	private ArrayList<User> admins;
 	private ArrayList<User> members;
+    private ArrayList<String> externalMembers;
 	private String starttime;
 	private String endtime;
 	private String description;
+    private int room;
 
-	public Meeting(User creator, String starttime, String endtime, String description){
+	public Meeting(User creator, String starttime, String endtime, String description, int room){
 		admins = new ArrayList<User>();
 		members = new ArrayList<User>();
 		admins.add(creator);
@@ -18,7 +26,10 @@ public class Meeting {
 		setStarttime(starttime);
 		setEndtime(endtime);
         setDescription(description);
+        setRoom(room);
 		meetingID = 0;
+
+        System.out.println(DATE_FORMAT.format(new Date()));
 	}
 	
 	public ArrayList<User> getAdmins() {
@@ -60,16 +71,36 @@ public class Meeting {
             this.members.remove(member);
         }
 	}
+
+    public ArrayList<String> getExternalMembers() {
+        return externalMembers;
+    }
+
+    public void addExternalMember(String member) {
+        if (externalMembers.contains(member)){
+            throw new IllegalArgumentException("The email is allready invited");
+        }else{
+            this.externalMembers.add(member);
+        }
+    }
+
+    public void deleteExternalMember(String member){
+        if (!externalMembers.contains(member)){
+            throw new IllegalArgumentException("The email is not a current member");
+        }else{
+            this.externalMembers.remove(member);
+        }
+    }
 	
 	public String getStarttime() {
 		return starttime;
 	}
 	
 	public void setStarttime(String starttime) {
-		if (starttime.matches("[0-9]+") && starttime.length() < 3 && Integer.parseInt(starttime) < 25 && Integer.parseInt(starttime) > 0) {
+		if (true) {
 			this.starttime = starttime;
 		}else{
-			throw new IllegalArgumentException("Invalid number.");
+			throw new IllegalArgumentException("Invalid starttime format.");
 		}
 	}
 	
@@ -78,15 +109,33 @@ public class Meeting {
 	}
 	
 	public void setEndtime(String endtime) {
-		if (endtime.matches("[0-9]+") && endtime.length() < 3 && Integer.parseInt(endtime) < 25 && Integer.parseInt(endtime) > 0) {
+		if (true) {
 			this.endtime = endtime;
 		}else{
-			throw new IllegalArgumentException("Invalid number.");
+			throw new IllegalArgumentException("Invalid endtime format.");
 		}
 	}
 	
-	public int getDuration() {
-		return Integer.parseInt(endtime) - Integer.parseInt(starttime);
+	public String getDuration() {
+        long minutes = -1;
+        long hours = -1;
+
+        try {
+            Date startTime = DATE_FORMAT.parse(starttime);
+            Date endTime = DATE_FORMAT.parse(endtime);
+
+            long diff = endTime.getTime() - startTime.getTime();
+
+            minutes = diff / (60 * 1000) % 60;
+            hours = diff / (60 * 60 * 1000) % 24;
+        }catch (Exception e){
+            throw new IllegalArgumentException("Error parsing dates.");
+        }
+
+        String hourString = (hours > 0 ? hours + (hours > 1 ? " hours" : " hour") : "");
+        String minuteString = (minutes > 0 ? minutes + (minutes > 1 ? " minutes" : " minute") : "");
+
+        return (hourString.equals("") ? minuteString : hourString + (minuteString.equals("") ? "" : ", " + minuteString));
 	}
 	
 	public String getDescription() {
@@ -96,6 +145,14 @@ public class Meeting {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
 	
 	public int getMeetingID() {
 		return meetingID;
@@ -107,4 +164,10 @@ public class Meeting {
 				+ ", endtime=" + endtime + ", duration=" + getDuration()
 				+ ", description=" + description + "]";
 	}
+
+    public static void main(String args[]){
+        Meeting meeting = new Meeting(new User("asdasd", "asdasd", "asdasd", "si@df.com", "12345678"), "2014-03-13 11:00:00", "2014-03-13 13:01:00", "Kjempegøy!", 1);
+
+        System.out.println(meeting);
+    }
 }
