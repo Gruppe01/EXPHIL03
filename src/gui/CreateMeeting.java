238bcf4fc14df2b240dc3a79e5e85be9c0555355
robinsjovoll@hotmail.com <robinss@stud.ntnu.no>
2014-03-13 11:30:38 +0100

    Create meeting screen made.

--- src/gui/CreateMeeting.java
+++ src/gui/CreateMeeting.java
@@ -9,6 +9,11 @@ import javax.swing.JSpinner;
 import javax.swing.JTextPane;
 import javax.swing.JList;
 import javax.swing.SwingConstants;
+
+import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
+import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
+import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
 import java.awt.Font;
 
 public class CreateMeeting extends JPanel {
@@ -24,6 +29,7 @@ public class CreateMeeting extends JPanel {
 	private JLabel label;
 	private JLabel label_1;
 	private JSpinner spinner_3;
+	private JScrollPane scrollPane_1;
 
 	/**
 	 * Create the panel.
@@ -62,7 +68,7 @@ public class CreateMeeting extends JPanel {
 		add(lblSelectTimeEnd);
 		
 		btnCancel = new JButton("Cancel");
-		btnCancel.setBounds(10, 333, 113, 45);
+		btnCancel.setBounds(10, 347, 113, 31);
 		add(btnCancel);
 		
 		btnEditMembers = new JButton("Edit Members");
@@ -70,7 +76,7 @@ public class CreateMeeting extends JPanel {
 		add(btnEditMembers);
 		
 		btnCreateMeeting = new JButton("Create meeting");
-		btnCreateMeeting.setBounds(461, 333, 113, 45);
+		btnCreateMeeting.setBounds(461, 347, 113, 31);
 		add(btnCreateMeeting);
 		
 		lblParticipants = new JLabel("Participants:");
@@ -104,5 +110,19 @@ public class CreateMeeting extends JPanel {
 		JTextPane textPane = new JTextPane();
 		textPane.setBounds(121, 103, 143, 86);
 		add(textPane);
+		
+		UtilDateModel model = new UtilDateModel();
+		JDatePanelImpl datePanel = new JDatePanelImpl(model);
+		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel);
+		datePicker.getJFormattedTextField().setText("Choose date here");
+		datePicker.setTextEditable(false);
+		datePicker.setToolTipText("");
+		datePicker.setBounds(274, 103, 145, 23);
+
+		add(datePicker);
+		
+//		scrollPane_1 = new JScrollPane();
+//		scrollPane_1.setBounds(274, 103, 132, 86);
+//		add(scrollPane_1);
 	}
 }
