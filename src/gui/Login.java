ad9afd9c1eee90f7f6834ed6ef31051bd75ca1a1
robinsjovoll@hotmail.com <robinss@stud.ntnu.no>
2014-03-13 12:06:16 +0100

    Removed frame from window and created a panel instead.

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,51 +1,22 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
-public class Login extends JFrame {
-
+public class Login extends JPanel {
 	private JPanel contentPane;
 	private JTextField usernameField;
 	private JPasswordField passwordField;
-
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
+	 * Create the panel.
 	 */
 	public Login() {
-		setResizable(false);
-		setTitle("Login");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		usernameField = new JTextField();
@@ -81,4 +52,5 @@ public class Login extends JFrame {
 		FYPButton.setBounds(121, 154, 200, 28);
 		contentPane.add(FYPButton);
 	}
+
 }
