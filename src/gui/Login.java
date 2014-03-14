65477aec269ee33415e71ec190c3088cc1108324
helliio <liang1503@live.no>
2014-03-14 12:18:54 +0100

    main and frame class generated

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -10,33 +10,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Login extends JPanel {
-	private JPanel contentPane;
 	private JTextField usernameField;
 	private JPasswordField passwordField;
 	/**
 	 * Create the panel.
 	 */
 	public Login() {
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(null);
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(null);
 		
 		usernameField = new JTextField();
 		usernameField.setBounds(121, 54, 200, 20);
-		contentPane.add(usernameField);
+		add(usernameField);
 		usernameField.setColumns(10);
 		
 		passwordField = new JPasswordField();
 		passwordField.setBounds(121, 89, 200, 20);
-		contentPane.add(passwordField);
+		add(passwordField);
 		
 		JLabel usernameLabel = new JLabel("Username:");
 		usernameLabel.setBounds(22, 56, 89, 17);
-		contentPane.add(usernameLabel);
+		add(usernameLabel);
 		
 		JLabel passwordLabel = new JLabel("Password:");
 		passwordLabel.setBounds(22, 91, 89, 17);
-		contentPane.add(passwordLabel);
+		add(passwordLabel);
 		
 		JButton loginButton = new JButton("Log in");
 		loginButton.addActionListener(new ActionListener() {
@@ -44,15 +42,15 @@ public class Login extends JPanel {
 			}
 		});
 		loginButton.setBounds(121, 120, 89, 23);
-		contentPane.add(loginButton);
+		add(loginButton);
 		
 		JButton newuserButton = new JButton("New user");
 		newuserButton.setBounds(232, 120, 89, 23);
-		contentPane.add(newuserButton);
+		add(newuserButton);
 		
 		JButton FYPButton = new JButton("Forgot your password?");
 		FYPButton.setBounds(121, 154, 200, 28);
-		contentPane.add(FYPButton);
+		add(FYPButton);
 	}
 
 }
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,19 +1,10 @@
 package gui;
 
+public class Main {
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+	public static void main(String[] args) {
+		Frame frame = new Frame();
 
-public class Main extends JPanel{
-	
-	public Main(){
-		
 	}
-	
-	public static void main(String args[]) {
-		JFrame frame = new JFrame("...");
-		frame.getContentPane().add(new Main());
-		frame.pack();
-		frame.setVisible(true);
-	}
+
 }
