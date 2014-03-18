7f0248c8529d5674d73dba5d7feaf02838490c67
helliio <liang1503@live.no>
2014-03-18 13:45:19 +0100

    Create user connected to model

--- src/gui/CreateUser.java
+++ src/gui/CreateUser.java
@@ -1,5 +1,7 @@
 package gui;
 
+import model.User;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -17,10 +19,14 @@ public class CreateUser extends JPanel {
 	private JPasswordField passwordField;
 	private JPasswordField repeatpasswordField;
 	private JButton confirmButton;
+	private User user;
+	private JTextField nameField;
+	private Frame frame;
 	/**
 	 * Create the panel.
 	 */
-	public CreateUser(final Frame frame) {
+	public CreateUser(final Frame in) {
+		frame = in;
 		setBorder(new EmptyBorder(5, 5, 5, 5));
 		setLayout(null);
 		
@@ -30,21 +36,21 @@ public class CreateUser extends JPanel {
 		usernameField.setColumns(10);
 		
 		emailField = new JTextField();
-		emailField.setBounds(136, 62, 200, 20);
+		emailField.setBounds(136, 93, 200, 20);
 		add(emailField);
 		emailField.setColumns(10);
 		
 		mobilenumberField = new JTextField();
-		mobilenumberField.setBounds(136, 93, 200, 20);
+		mobilenumberField.setBounds(136, 124, 200, 20);
 		add(mobilenumberField);
 		mobilenumberField.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(136, 127, 200, 20);
+		passwordField.setBounds(136, 158, 200, 20);
 		add(passwordField);
 		
 		repeatpasswordField = new JPasswordField();
-		repeatpasswordField.setBounds(136, 158, 200, 20);
+		repeatpasswordField.setBounds(136, 189, 200, 20);
 		add(repeatpasswordField);
 		
 		JLabel usernameLabel = new JLabel("Username:");
@@ -52,23 +58,32 @@ public class CreateUser extends JPanel {
 		add(usernameLabel);
 		
 		JLabel emailLabel = new JLabel("Email:");
-		emailLabel.setBounds(10, 65, 116, 14);
+		emailLabel.setBounds(10, 96, 116, 14);
 		add(emailLabel);
 		
 		JLabel mobilenumberLabel = new JLabel("Mobile number:");
-		mobilenumberLabel.setBounds(10, 96, 116, 14);
+		mobilenumberLabel.setBounds(10, 127, 116, 14);
 		add(mobilenumberLabel);
 		
 		JLabel passwordLabel = new JLabel("Password:");
-		passwordLabel.setBounds(10, 127, 116, 14);
+		passwordLabel.setBounds(10, 158, 116, 14);
 		add(passwordLabel);
 		
 		JLabel repeatpasswordLabel = new JLabel("Repeat password:");
-		repeatpasswordLabel.setBounds(10, 161, 116, 14);
+		repeatpasswordLabel.setBounds(10, 192, 116, 14);
 		add(repeatpasswordLabel);
 		
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(10, 65, 116, 14);
+		add(lblName);
+		
+		nameField = new JTextField();
+		nameField.setColumns(10);
+		nameField.setBounds(136, 62, 200, 20);
+		add(nameField);
+		
 		JButton cancelButton = new JButton("Cancel");
-		cancelButton.setBounds(39, 206, 93, 29);
+		cancelButton.setBounds(39, 226, 93, 29);
 		add(cancelButton);
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -77,13 +92,43 @@ public class CreateUser extends JPanel {
 		});
 		
 		confirmButton = new JButton("Confirm");
-		confirmButton.setBounds(314, 206, 93, 29);
+		confirmButton.setBounds(315, 226, 93, 29);
 		add(confirmButton);
 		confirmButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				frame.setFrame("login");
+				create();
 			}
 		});
 	}
-
+	
+	private void create(){
+		String username;
+		String password;
+		String conPassword;
+		String name;
+		String email;
+		String phonenumber;
+		
+		username = usernameField.getText();
+		password = String.valueOf(passwordField.getPassword());
+		conPassword = String.valueOf(repeatpasswordField.getPassword());
+		name = nameField.getText();
+		email = emailField.getText();
+		phonenumber = mobilenumberField.getText();
+		
+		if (!password.equals(conPassword)){
+			ErrorMessage error = new ErrorMessage("Error", "Password and Repeat password Don't match");
+			return;
+		}
+		
+		try{
+		user = new User(username, password, name, email, phonenumber);
+		}
+		catch(IllegalArgumentException e){
+			ErrorMessage error = new ErrorMessage("Error", e.getMessage());
+			return;
+		}
+		
+		frame.setFrame("login");
+	}
 }
