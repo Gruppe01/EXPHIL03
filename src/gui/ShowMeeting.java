5350eb94bb84f2b82c65d3bcc36fcd91518bd625
robinsjovoll@hotmail.com <robinss@stud.ntnu.no>
2014-03-20 18:12:30 +0100

    Changed gui

--- src/gui/ShowMeeting.java
+++ src/gui/ShowMeeting.java
@@ -5,6 +5,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.SwingConstants;
 import javax.swing.JList;
@@ -17,6 +20,7 @@ import javax.swing.border.*;
 import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
 import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
 import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
 import javax.swing.JFormattedTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
@@ -26,86 +30,110 @@ public class ShowMeeting extends JPanel {
 	private JTable table;
 
 	
-	public ShowMeeting() {
+	public ShowMeeting(final Frame frame) {
 		setLayout(null);
 		
 		JLabel lblDescription = new JLabel("Description:");
-		lblDescription.setBounds(130, 58, 72, 14);
+		lblDescription.setBounds(10, 11, 72, 14);
 		add(lblDescription);
 		
 		JTextPane textPane = new JTextPane();
 		textPane.setEditable(false);
-		textPane.setBounds(212, 52, 123, 69);
+		textPane.setBounds(110, 11, 147, 69);
 		add(textPane);
 		
 		JLabel lblPlace = new JLabel("Place:");
-		lblPlace.setBounds(130, 138, 46, 14);
+		lblPlace.setBounds(10, 97, 46, 14);
 		add(lblPlace);
 		
 		JTextPane textPane_1 = new JTextPane();
 		textPane_1.setEditable(false);
-		textPane_1.setBounds(212, 132, 123, 20);
+		textPane_1.setBounds(110, 91, 147, 20);
 		add(textPane_1);
 		
 		JLabel lblStartTime = new JLabel("Start time:");
-		lblStartTime.setBounds(130, 169, 72, 14);
+		lblStartTime.setBounds(10, 128, 72, 14);
 		add(lblStartTime);
 		
 		JTextPane textPane_2 = new JTextPane();
-		textPane_2.setBounds(212, 163, 123, 20);
+		textPane_2.setBounds(110, 122, 147, 20);
 		add(textPane_2);
 		
 		JLabel lblEndTime = new JLabel("End time:");
-		lblEndTime.setBounds(130, 200, 72, 14);
+		lblEndTime.setBounds(10, 159, 72, 14);
 		add(lblEndTime);
 		
 		JTextPane textPane_3 = new JTextPane();
-		textPane_3.setBounds(212, 194, 123, 20);
+		textPane_3.setBounds(110, 153, 147, 20);
 		add(textPane_3);
 		
 		JButton btnCancel = new JButton("Back");
 		btnCancel.setBounds(10, 287, 89, 23);
 		add(btnCancel);
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setFrame("mainScreen");
+			}
+		});
 		
-		JButton btnNewButton = new JButton("Show members");
-		btnNewButton.setBounds(130, 287, 110, 23);
-		add(btnNewButton);
-		
-		JCheckBox chckbxShowInCalendar = new JCheckBox("Show in calendar");
-		chckbxShowInCalendar.setBounds(263, 287, 110, 23);
-		add(chckbxShowInCalendar);
+		JButton btnShowMembers = new JButton("Show members");
+		btnShowMembers.setBounds(130, 287, 127, 23);
+		add(btnShowMembers);
+		btnShowMembers.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ShowMembers showMembers = new ShowMembers();
+				showMembers.setVisible(true);
+			}
+		});
 		
 		JButton btnAccept = new JButton("Accept");
-		btnAccept.setBounds(470, 253, 89, 23);
+		btnAccept.setBounds(480, 268, 89, 42);
 		add(btnAccept);
+		btnAccept.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setFrame("mainScreen");
+			}
+		});
 		
 		JButton btnDecline = new JButton("Decline");
-		btnDecline.setBounds(470, 287, 89, 23);
+		btnDecline.setBounds(381, 268, 89, 42);
 		add(btnDecline);
+		btnDecline.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setFrame("mainScreen");
+			}
+		});
 		
 
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Time before meeting", "10 min", "15 min", "20 min", "25 min", "30 min"}));
-		comboBox.setBounds(212, 225, 123, 20);
+		comboBox.setBounds(110, 184, 147, 20);
 		add(comboBox);
 		
 		JLabel lblCreatAlaram = new JLabel("Create alarm:");
-		lblCreatAlaram.setBounds(130, 225, 72, 14);
+		lblCreatAlaram.setBounds(10, 190, 89, 14);
 		add(lblCreatAlaram);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"SMS", "Email", "Both"}));
-		comboBox_1.setBounds(345, 225, 57, 20);
-		add(comboBox_1);
-		
 		JLabel lblDate = new JLabel("Date:");
-		lblDate.setBounds(381, 58, 46, 14);
+		lblDate.setBounds(267, 11, 46, 14);
 		add(lblDate);
 		
 		JTextPane textPane_4 = new JTextPane();
 		textPane_4.setEditable(false);
-		textPane_4.setBounds(421, 58, 123, 20);
+		textPane_4.setBounds(312, 5, 123, 20);
 		add(textPane_4);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(267, 42, 302, 162);
+		add(scrollPane);
+		
+		JLabel lblMeetingMembers = new JLabel("Meeting members");
+		lblMeetingMembers.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMeetingMembers.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		scrollPane.setColumnHeaderView(lblMeetingMembers);
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
 		
 		
 
